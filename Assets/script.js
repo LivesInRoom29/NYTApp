6300b77f3232a807fb16759232da3af8afce922c
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-07-25 12:47:28 -0400

    Added query link to NYT API

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,11 @@
 //API key for nyt: vW98uWc5IvP1AlTsCIGOekDaEbGr0fNE
+$(document).ready(function() {
+    // Need event listeners for buttons
+    // Need $.get for url with search terms
+
+    const queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerms}&api-key=vW98uWc5IvP1AlTsCIGOekDaEbGr0fNE`
+
+    // const searchTerms will come from the input field
+}
+
+
